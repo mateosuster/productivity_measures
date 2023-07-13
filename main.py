21109714e1ad9f7793bf4a733161effb9c01b8c8
mateosuster <msuster@nisum.com>
2023-07-13 13:45:47 -0300

    se agrega if para no ejecutar el preprocesamiento

--- main.py
+++ main.py
@@ -9,24 +9,35 @@ from src.functions import *
 #datos de contienntes
 continents = pd.read_csv("data/bea/Countries-Continents.csv")
 
-#all_parents
-path = "data/bea/all_parent"
 
-data_2dig =create_data(path, type= 'all_parent')
-data_4dig =create_data(path, type= 'all_parent', desagregation = "4dig")
+file_path_all_parent_2dig = "results/bea/all_parent/all_parent_2dig.csv"
+file_path_all_parent_4dig = "results/bea/all_parent/all_parent_4dig.csv"
+file_path_maj_own = "results/bea/majority_owned_nonbank/data_majority_owned_nonbank.csv"
 
-data_2dig = calculos(data_2dig, type= 'all_parent')
-data_4dig = calculos(data_4dig, type= 'all_parent')
+if (not os.path.exists(file_path_all_parent_2dig)) and  (not os.path.exists(file_path_all_parent_4dig)) and  (not os.path.exists(file_path_maj_own))  :
+    #all_parents
+    path = "data/bea/all_parent"
 
-data_2dig.to_csv("results/bea/all_parent/all_parent_2dig.csv")
-data_4dig.to_csv("results/bea/all_parent/all_parent_4dig.csv")
+    data_2dig =create_data(path, type= 'all_parent')
+    data_4dig =create_data(path, type= 'all_parent', desagregation = "4dig")
+
+    data_2dig = calculos(data_2dig, type= 'all_parent')
+    data_4dig = calculos(data_4dig, type= 'all_parent')
+
+    data_2dig.to_csv(file_path_all_parent_2dig)
+    data_4dig.to_csv(file_path_all_parent_4dig)
 
 
-#mayority_owned
-path = "data/bea/majority_owned_nonbank"
+    #mayority_owned
+    path = "data/bea/majority_owned_nonbank"
 
-data=create_data(path, type= 'majority_owned')
-data = calculos(data,type= 'majority_owned')
+    data=create_data(path, type= 'majority_owned')
+    data = calculos(data,type= 'majority_owned')
 
-data.merge( continents, how= "left", left_on="country", right_on="Country"   ).drop("Country", axis=1).to_csv("results/bea/majority_owned_nonbank/data_majority_owned_nonbank.csv", index=False)
+    data = data.merge( continents, how= "left", left_on="country", right_on="Country"   ).drop("Country", axis=1)
+    data.to_csv(file_path_maj_own, index=False)
+else:
+    print("################ Files already exists ################ ")
+    data = pd.read_csv(file_path_maj_own)
 
+# print(data.loc[ data['Continent'].isnull() , ['country', 'Continent']].drop_duplicates() )
